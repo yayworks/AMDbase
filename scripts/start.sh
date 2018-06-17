b6597877a2c0ce345d1e6b3803bbbd3164812200
yayworks <raj@yayworks.com>
2018-06-17 07:34:11 -0500

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,6 +3,7 @@
 #cd /data
 
 exec /usr/local/bin/nimbix_desktop /usr/local/AMDuProf_Linux_x64_1.2.275/bin/AMDuProf
+exec /sbin/init
 #exec /usr/local/bin/nimbix_desktop octave --force-gui
 #exec /usr/local/bin/nimbix_desktop /usr/local/ParaView-5.4.1/bin/paraview
 
